35caef109fb8e41497a9fffe078ffb29fbec9003
Giuliano Vallone <giuliano.vallone@ikmail.com>
2025-06-17 12:04:11 +0200

    Cambiato nome alla funzione proxy

--- proxy_tls.c
+++ proxy_tls.c
@@ -12,77 +12,7 @@
 #include "proxy_tls.h"
 
 
-void tcp_client_handler(int client_fd, int service_port, char *root_ca_path)
-{
-        Socket service_client;
-        SSL_CTX *ctx;
-        SSL *ssl_service_client;
-
-        service_client = create_client("127.0.0.1", service_port);
-
-        ctx = create_client_context_tls(root_ca_path);
-
-        if (ctx == NULL) {
-                close_socket(service_client);
-                exit(EXIT_FAILURE);
-        }
-
-        ssl_service_client = SSL_new(ctx);
-
-        SSL_set_fd(ssl_service_client, service_client.fd);
-
-        printf("[*] TCP Server: Trying to connect to the service...\n");
-
-        if (SSL_connect(ssl_service_client) <= 0) {
-                ERR_print_errors_fp(stderr);
-                SSL_free(ssl_service_client);
-                SSL_CTX_free(ctx);
-                close_socket(service_client);
-                exit(EXIT_FAILURE);
-        }
-
-        printf("[*] Successfully connected to the service via TLS.\n");
-
-        proxy(ssl_service_client, client_fd);
-
-        SSL_free(ssl_service_client);
-        SSL_shutdown(ssl_service_client);
-        close_socket(service_client);
-}
-
-
-void tls_client_handler(int client_fd, char *cert_path, char *key_path, int tcp_server_port)
-{
-        SSL_CTX *ctx = create_tls_server_context(cert_path, key_path);
-        SSL *ssl_client;
-
-        Socket client_tcp;
-
-        ssl_client = SSL_new(ctx);
-        SSL_set_fd(ssl_client, client_fd);
-
-        if (SSL_accept(ssl_client) <= 0) {
-                ERR_print_errors_fp(stderr);
-                SSL_free(ssl_client);
-                close(client_fd);
-        }
-
-        
-        client_tcp = create_client("127.0.0.1", tcp_server_port);
-
-        printf("[*] TLS Server: Successfully connected to server TCP\n");
-
-        proxy(ssl_client, client_tcp.fd);
-
-        SSL_shutdown(ssl_client);
-        SSL_free(ssl_client);
-        SSL_CTX_free(ctx);
-        EVP_cleanup();
-        close_socket(client_tcp);
-}
-
-
-void proxy(SSL *ssl_socket, int socket_fd)
+void forward(SSL *ssl_socket, int socket_fd)
 {
         struct pollfd fds[2];
         char buffer[BUFFER_SIZE + 1];
@@ -120,3 +50,72 @@ void proxy(SSL *ssl_socket, int socket_fd)
         }
 }
 
+
+void tcp_client_handler(int client_fd, int service_port, char *root_ca_path)
+{
+        Socket service_client;
+        SSL_CTX *ctx;
+        SSL *ssl_service_client;
+
+        service_client = create_client("127.0.0.1", service_port);
+
+        ctx = create_client_context_tls(root_ca_path);
+
+        if (ctx == NULL) {
+                close_socket(service_client);
+                exit(EXIT_FAILURE);
+        }
+
+        ssl_service_client = SSL_new(ctx);
+
+        SSL_set_fd(ssl_service_client, service_client.fd);
+
+        printf("[*] TCP Server: Trying to connect to the service...\n");
+
+        if (SSL_connect(ssl_service_client) <= 0) {
+                ERR_print_errors_fp(stderr);
+                SSL_free(ssl_service_client);
+                SSL_CTX_free(ctx);
+                close_socket(service_client);
+                exit(EXIT_FAILURE);
+        }
+
+        printf("[*] Successfully connected to the service via TLS.\n");
+
+        forward(ssl_service_client, client_fd);
+
+        SSL_free(ssl_service_client);
+        SSL_shutdown(ssl_service_client);
+        close_socket(service_client);
+}
+
+
+void tls_client_handler(int client_fd, char *cert_path, char *key_path, int tcp_server_port)
+{
+        SSL_CTX *ctx = create_tls_server_context(cert_path, key_path);
+        SSL *ssl_client;
+
+        Socket client_tcp;
+
+        ssl_client = SSL_new(ctx);
+        SSL_set_fd(ssl_client, client_fd);
+
+        if (SSL_accept(ssl_client) <= 0) {
+                ERR_print_errors_fp(stderr);
+                SSL_free(ssl_client);
+                close(client_fd);
+        }
+
+        
+        client_tcp = create_client("127.0.0.1", tcp_server_port);
+
+        printf("[*] TLS Server: Successfully connected to server TCP\n");
+
+        forward(ssl_client, client_tcp.fd);
+
+        SSL_shutdown(ssl_client);
+        SSL_free(ssl_client);
+        SSL_CTX_free(ctx);
+        EVP_cleanup();
+        close_socket(client_tcp);
+}
